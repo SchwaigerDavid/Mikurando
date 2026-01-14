1fb491c0cec653a415d7263c8c6e873625ef099b
thomasstu <thomasstu@edu.aau.at>
2026-01-15 00:26:16 +0100

    Update app.routes.ts for owner
    Add imports + paths + components for owner

--- mikurando-angular/src/app/app.routes.ts
+++ mikurando-angular/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { RestaurantProfile } from './owner/restaurant-profile/restaurant-profile';
+import { OrderReception } from './owner/order-reception/order-reception';
 import { authGuard } from './shared/auth/auth.guard';
 
 export const routes: Routes = [
@@ -19,6 +21,15 @@ export const routes: Routes = [
     path: 'site-manager',
     canActivate: [authGuard],
     loadChildren: () => import('./site-manager').then((m) => m.siteManagerRoutes),
+  },
+
+  {
+    path: 'owner/restaurant-profile',
+    component: RestaurantProfile
+  },
+  {
+    path: 'owner/orders',
+    component: OrderReception
   },
 
   { path: '**', redirectTo: '/login' },
