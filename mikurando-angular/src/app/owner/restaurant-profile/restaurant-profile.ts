b8cb054de03109461725d1d8db9c7724601a3f4a
thomasstu <thomasstu@edu.aau.at>
2026-01-15 22:50:31 +0100

    Update restaurant-profile
    Added necessary parameters

--- mikurando-angular/src/app/owner/restaurant-profile/restaurant-profile.ts
+++ mikurando-angular/src/app/owner/restaurant-profile/restaurant-profile.ts
@@ -27,12 +27,65 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class RestaurantProfile {
   profileForm = new FormGroup({
-    name: new FormControl('David Pizza', Validators.required),
-    phone: new FormControl('0123456789', Validators.required)
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    adress: new FormControl('', Validators.required), //name korrekt so
+    area_code: new FormControl('', Validators.required),
+    customer_notes: new FormControl('-'),
+    min_order_value: new FormControl('', Validators.required),
+    category: new FormControl('', Validators.required),
+
   });
+
+  get name() {
+    return this.profileForm.get('name');
+  }
+
+  get description() {
+    return this.profileForm.get('description');
+  }
+
+  get adress() {
+    return this.profileForm.get('adress');
+  }
+
+  get areaCode() {
+    return this.profileForm.get('area_code');
+  }
+
+  get customerNotes() {
+    return this.profileForm.get('customer_notes');
+  }
+
+  get minOrderValue() {
+    return this.profileForm.get('min_order_value');
+  }
+
+  get category() {
+    return this.profileForm.get('category');
+  }
+
+
+
+
+
+  //approved bool
+  //is_active bool
+  //id generieren für restaurant
+  //owner id zuordnen
 
   save() {
     console.log('Form value:', this.profileForm.value);
+
+    const payload = {
+      name: this.name,
+      description: this.description,
+      adress: this.adress,
+      customerNotes: this.customerNotes,
+      minorderValue: this.minOrderValue,
+      category: this.category,
+      restaurantID: 0, //get unique id
+    }
   }
 }
 
