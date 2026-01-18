129e34fce69936469758c5a2e178befab9c8386a
handlerc-dev <163014500+chhandler@users.noreply.github.com>
2026-01-18 22:09:05 +0100

    fix auth interceptor

--- mikurando-angular/src/app/app.config.ts
+++ mikurando-angular/src/app/app.config.ts
@@ -1,17 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from './shared/auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(),
-    provideAnimations(),
-  ]
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
